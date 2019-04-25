a7fa7117a23f2327040397557f435ffb6917f4f9
Oliver Barnwell <barnwelloliver@gmail.com>
2019-04-25 16:52:51 +0100

    mongodb switch

--- index.js
+++ index.js
@@ -14,32 +14,33 @@ const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cluster0-nrdwp.gcp.mongodb.
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("leaderboard").collection("highscores");
-  // perform actions on the collection object
+
+  // add some security-related headers to the response
+  app.use(helmet())
+  // parse JSON strings received in req body as JS object
+  app.use(express.json())
+
+  app.get('*', (req, res) => {
+      res.set('Content-Type', 'text/html')
+      res.status(200).send(`
+        Pac-Man?
+      `)
+  })
+
+  // Get top ten leaderboard entries in descending order of score
+  app.get('leaderboard', (req, res) => {
+    res.json({})
+  })
+
+  // Allow for users to POST new scores
+  app.post('leaderboard/:name/:score', (req, res) => {
+    const name = req.params.name;
+    const score = req.params.score;
+    res.json({name: name, score: score})
+  })
+
   client.close();
 });
 
-// add some security-related headers to the response
-app.use(helmet())
-// parse JSON strings received in req body as JS object
-app.use(express.json())
-
-app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
-    res.status(200).send(`
-      Pac-Man?
-    `)
-})
-
-// Get top ten leaderboard entries in descending order of score
-app.get('leaderboard', (req, res) => {
-  res.json({})
-})
-
-// Allow for users to POST new scores
-app.post('leaderboard/:name/:score', (req, res) => {
-  const name = req.params.name;
-  const score = req.params.score;
-  res.json({name: name, score: score})
-})
 
 module.exports = app
