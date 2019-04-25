3ddd1a9ea0388855df64bea373d444095e5db1bf
Oliver Barnwell <barnwelloliver@gmail.com>
2019-04-25 22:05:58 +0100

    working

--- index.js
+++ index.js
@@ -11,34 +11,39 @@ const mongoUser = process.env.MONGO_USER;
 const mongoPass = process.env.MONGO_PASS;
 const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cluster0-nrdwp.gcp.mongodb.net/test?retryWrites=true`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const mongoclient = new MongoClient(uri, { useNewUrlParser: true });
 
 // add some security-related headers to the response
 app.use(helmet())
 
-app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
-    res.status(200).send(`
-      Pac-Man?
-    `)
-})
-
-// Get top ten leaderboard entries in descending order of score
-app.get('leaderboard', (req, res) => {
-  res.json({})
-})
-
 // Allow for users to POST new scores
-app.get('leaderboard/:name/:score', async (req, res) => {
-  const name = req.params.name
-  const score = req.params.score
-  if(name && score > -1) {
-    const entry = {name: name, score: score}
-    await client.connect()
-    const highscores = client.db("leaderboard").collection("highscores")
-    highscores.save(entry)
-  }
+app.post('*', (req, res) => {
+  const name = req.query.name
+  const score = req.query.score
+  console.log(req.param, req.query)
+  mongoclient.connect((err, client) => {
+    const db = client.db("leaderboard")
+    const highscores = db.collection("highscores")
+    console.log(name, score)
+    if(name && score) {
+      const entry = {name: name, score: score}
+      highscores.insertOne(entry, (err, r) => {
+        client.close()
+        res.json(r)
+      })
+    }
+  })
 })
 
+app.get('*', (req, res) => {
+  mongoclient.connect((err, client) => {
+    const db = client.db("leaderboard")
+    // return the highest score
+    db.collection("highscores").find().sort({score:-1}).limit(1).toArray((err, result) => {
+      if(err) throw err
+      res.json(result)
+    })
+  })
+})
 
 module.exports = app
